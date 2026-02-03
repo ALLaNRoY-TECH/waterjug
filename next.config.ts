eb52f64b46bc5498a24cc0aab02c3abed7360f9a
Aroy____ <allansolly9391@gmail.com>
2026-02-03 04:23:23 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1770091067001.cluster-ancjwrkgr5dvux4qug5rbzyc2y.cloudworkstations.dev',
+    ],
+  },
   images: {
     remotePatterns: [
       {
